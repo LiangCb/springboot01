7bf4021e7707ff5b9bf37f28c21e8f90ca33e681
changbing.liang <162439793@qq.com>
2020-11-11 22:09:01 +0800

    project initial

---
+++ src/main/java/com/demo/HelloMain.java
@@ -0,0 +1,15 @@
+package com.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author: cbliang
+ * @Date: 2020/9/18 15:38
+ */
+@SpringBootApplication
+public class HelloMain {
+    public static void main(String[] args) {
+        SpringApplication.run(HelloMain.class,args);
+    }
+}
---
+++ src/main/java/com/demo/controller/HelloController.java
@@ -0,0 +1,21 @@
+package com.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Author: cbliang
+ * @Date: 2020/9/18 15:39
+ */
+@RestController
+public class HelloController {
+
+    @RequestMapping(value = "/hello")
+    public String hello(){
+        return "Hello boot";
+    }
+
+}
